aae9a9813b5f9c76fc1235a26d593f23c198a74e
iamseungjun <leezeunjun@gmail.com>
2021-01-20 20:52:00 +0900

    기본 수학 2 완료

--- Backjoon/Backjoon_1002.cpp
+++ Backjoon/Backjoon_1002.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
 #include <cmath>
 
+void swap(int *a, int *b) {
+    int t;
+    t=*a;
+    *a=*b;
+    *b=t;
+}
+
 int main() {
-    int n, x1, x2, y1, y2, r1, r2, i;
+    int n, x1, x2, y1, y2, r1, r2, r, i;
 
     scanf("%d", &n);
     for(i=0; i<n; i++) {
         scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
-        int d=abs(x1-x2)+abs(y1-y2);
-        printf("%d %d\n", d, r1+r2);
+
+        if(r1<r2) {
+            swap(&x1, &x2);
+            swap(&y1, &y2);
+            swap(&r1, &r2);
+        }
+
+        double d=sqrt(pow(x1-x2, 2)+pow(y1-y2, 2));
+
+        if(x1==x2 && y1==y2 && r1==r2) {
+            r=-1;
+        } else if(x1==x2 && y1==y2) {
+            r=0;
+        } else if(d>r1+r2 || r1-r2>d) {
+            r=0;
+        } else if(r1+r2==d || r1-r2==d) {
+            r=1;
+        } else {
+            r=2;
+        }
+        printf("%d\n", r);
+
     }
 
     return 0;
---
+++ Backjoon/Backjoon_10870.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int f(int n) {
+    if(n<=1) {
+        return n;
+    } else {
+        return f(n-1)+f(n-2);
+    }
+}
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+    printf("%d", f(n));
+
+    return 0;
+}
